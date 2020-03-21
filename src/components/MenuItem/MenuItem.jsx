f59e200b2bf3a3f17820270342cee4b7590a2735
Alejandro Ferrero <alejandroferrero@outlook.com>
2020-03-21 18:52:00 +0100

    Broken down HomePage into individual components and added styling

---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './MenuItem.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+	<div className={`${size} menu-item`}>
+		<div
+			className="background-image"
+			style={{
+				backgroundImage: `url(${imageUrl})`,
+			}}
+		/>
+		<div className="content"> 
+			<h1 className="title">{title.toUpperCase()}</h1>
+			<span className="subtitle"> SHOP NOW </span>
+		</div>
+	</div>
+);
+
+MenuItem.propTypes = {
+	title: PropTypes.string.isRequired,
+	imageUrl: PropTypes.string.isRequired,
+	size: PropTypes.string,
+};
+
+MenuItem.defaultProps = {
+	size: undefined,
+};
+
+export default MenuItem;
