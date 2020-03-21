3750b19a37d2553d7f2f06f0688cb5429cea8c1e
Alejandro Ferrero <alejandroferrero@outlook.com>
2020-03-21 14:44:43 +0100

    Projec setup

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": ["airbnb", "plugin:react/recommended"],
+    "rules": {
+      "no-console": 0,
+      "import/no-dynamic-require": 0,
+      "func-names": ["error", "never"],
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      "indent": [2, "tab"],
+      "no-tabs": 0,
+      "arrow-parens": ["error", "as-needed"],
+      "react/jsx-indent": "off",
+      "react/jsx-indent-props": "off",
+    }
+  };
