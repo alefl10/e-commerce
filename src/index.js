0b73e90db029ba4f36f9f0237424bce1a77b2518
Alejandro Ferrero <alejandroferrero@outlook.com>
2020-03-28 20:36:08 +0100

    Added redux persistance for frontend local storage

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
+/* *** REDUX *** */
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 /* *** COMPONENTS *** */
 import App from './App';
@@ -14,7 +17,9 @@ import './index.css';
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</BrowserRouter>
 	</Provider>,
 	document.getElementById('root'),
