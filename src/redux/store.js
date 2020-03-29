d3c457c5770aad7f5b2eb0dbbb4f1f9ad94b8742
Alejandro Ferrero <alejandroferrero@outlook.com>
2020-03-29 21:38:44 +0200

    Conditionally injected redux logger depending on NODE_ENV

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,8 @@ import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 
-const middlewares = [logger];
+// create-react-app sets NODE_ENV to development automatically when building locally
+const middlewares = process.env.NODE_ENV === 'development' ? [logger] : [];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
 export const persistor = persistStore(store);
-
-export default { persistor, store };
